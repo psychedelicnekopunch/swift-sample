ff0dc966abb813df8e056f5e50873e048fbfd57e
psychedelicnekopunch <psychedelic.nekopunch@gmail.com>
2024-08-31 17:21:02 +0900

    commit

---
+++ swift-sample/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  swift-sample
+//
+//  Created on 2024/08/30.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ swift-sample/swift_sampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_sampleApp.swift
+//  swift-sample
+//
+//  Created on 2024/08/30.
+//
+
+import SwiftUI
+
+@main
+struct swift_sampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
